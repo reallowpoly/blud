f47be1acce645f92840f49f4b7acb2b6ae021eb3
reallowpoly <ajay.vikram2110@gmail.com>
2025-07-19 21:37:46 +0530

    add main src

---
+++ src/main/java/com/lonewalkerstudios/blud/bludClient.java
@@ -0,0 +1,10 @@
+package com.lonewalkerstudios.blud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class bludClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
