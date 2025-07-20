12b5cdcd3890e2c1de56e76ad50add896222a254
reallowpoly <ajay.vikram2110@gmail.com>
2025-07-20 15:18:15 +0530

    update ModItems.java + Blud.java

--- src/main/java/com/lonewalkerstudios/blud/Blud.java
+++ src/main/java/com/lonewalkerstudios/blud/Blud.java
@@ -1,5 +1,6 @@
 package com.lonewalkerstudios.blud;
 
+import com.lonewalkerstudios.blud.items.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -15,10 +16,7 @@ public class Blud implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/lonewalkerstudios/blud/items/ModItems.java
@@ -0,0 +1,25 @@
+package com.lonewalkerstudios.blud.items;
+
+import com.lonewalkerstudios.blud.Blud;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+public static final Item BLUD_WOOD = registerItem(new Item (new Item.Settings()));
+    private static Item registerItem(Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Blud.MOD_ID, "blud_wood"),item);
+    }
+
+    public static void registerModItems() {
+        Blud.LOGGER.info("Killllll myselffffff" + Blud.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(BLUD_WOOD);
+        });
+    }
+}
